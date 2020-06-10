308c437652ec73605e23c51b955b9c788e8c77ab
miguelhdez <miguel.hdez.sob@hotmail.com>
2020-06-09 21:29:27 -0500

    solved first two problems

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -1,12 +1,20 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Scanner;
+
 public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
 
   public static short countBits(int x) {
-    // TODO - you fill in here.
-    return 0;
+
+    short numBits = 0;
+    while (x != 0) {
+      numBits += (x & 1);
+      x >>>= 1;
+    }
+    return numBits;
   }
 
   public static void main(String[] args) {
--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -4,8 +4,12 @@ import epi.test_framework.GenericTest;
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    short numBits = 0;
+    while (x != 0) {
+      numBits += (x & 1);
+      x >>>= 1;
+    }
+    return (short) (numBits % 2);
   }
 
   public static void main(String[] args) {
